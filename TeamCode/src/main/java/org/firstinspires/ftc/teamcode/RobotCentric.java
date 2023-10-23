b5e1d97507bb37a3d2bd7cdec3dc3b78daa6b4c9
Arav-R <63694299+Arav-R@users.noreply.github.com>
2023-10-23 16:28:45 -0500

    fixed hardwaremaps

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCentric.java
@@ -11,10 +11,10 @@ public class RobotCentric extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("leftFront");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("leftBack");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("rightFront");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("rightBack");
 
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
